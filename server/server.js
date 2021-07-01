d8772213b0e6b7c0b74cb2a750d98a4b049d8100
Joey Huntington <jwhunt19@gmail.com>
2021-07-01 14:48:55 -0400

    Add API handling on server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+app.get('/api/list', (req, res) => {
+  axios.get('https://fetch-hiring.s3.amazonaws.com/hiring.json')
+    .then(({ data }) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(err.response.status).send(err);
+    });
+});
 
 app.listen(process.env.PORT || 3000);
