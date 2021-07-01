a9362c0ca18d03a7261556fc2678db115c3ff280
Joey Huntington <jwhunt19@gmail.com>
2021-07-01 17:48:46 -0400

    Remove unused imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,6 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import {
-//   Grid, List, ListItem, ListItemText, ListSubheader,
-// } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   TableContainer, Table, TableCell, TableRow, TableHead, TableBody, Paper,
